1e47c30a1c7f28cc469eadaaa9325b4cd8190133
Panagiotis Bekris <pbekris8@gmail.com>
2026-01-13 01:16:18 +0200

    feat: add new method implementation in AccountReadOnlyDTO in bankapp, in ch18

--- src/gr/aueb/cf/cf9/ch18/bankapp/dto/AccountReadOnlyDTO.java
+++ src/gr/aueb/cf/cf9/ch18/bankapp/dto/AccountReadOnlyDTO.java
@@ -3,4 +3,11 @@ package gr.aueb.cf.cf9.ch18.bankapp.dto;
 import java.math.BigDecimal;
 
 public record AccountReadOnlyDTO(String iban, BigDecimal balance) {
+
+    //Override την toString για να εκτυπώνω με δικό μου format όπου χρειάζομαι στην εφαρμογή
+
+    @Override
+    public String toString() {
+        return "ΙΒΑΝ: " +   iban + ", Balance: " + balance;
+    }
 }
